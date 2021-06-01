e89f20e7886d44cfc1b7b49d1615f4a8809b2408
hainguyen <hainguyen56211@gmail.com>
2021-06-01 17:27:21 +0700

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5030;
 
 const loginRoute = require("./routes/login.route");
 const apiRoute = require("./routes/api.route");
